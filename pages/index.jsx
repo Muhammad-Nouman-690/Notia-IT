898ea6c9e1e612db148b601284c6cbe648ef3b33
Muhammad-Nouman-690 <nomi690@hotmail.com>
2021-02-17 19:30:34 +0500

    Blog website in Next.Js

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,22 +2,42 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          An enthusiastic developer who works with extreme dedication to fulfill
-          expectation of client.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
       <h1>
         Welcome Nouman to
@@ -26,5 +46,27 @@ export default function Home() {
         </Link>
       </h1>
     </Layout>
+
+    // <Layout home>
+    //   <Head>
+    //     <title>{siteTitle}</title>
+    //   </Head>
+    //   <section className={utilStyles.headingMd}>
+    //     <p>
+    //       An enthusiastic developer who works with extreme dedication to fulfill
+    //       expectation of client.
+    //     </p>
+    //     <p>
+    //       (This is a sample website - you’ll be building a site like this on{" "}
+    //       <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+    //     </p>
+    //   </section>
+    //   <h1>
+    //     Welcome Nouman to
+    //     <Link href="posts/first-post">
+    //       <a> My First Page </a>
+    //     </Link>
+    //   </h1>
+    // </Layout>
   );
 }
